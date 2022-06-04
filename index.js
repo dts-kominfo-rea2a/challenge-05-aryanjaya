4cbb17bd6e93cd6deba885ba7c7902ea835ca52e
Aryan Jaya <aryanjaya88@gmail.com>
2022-06-04 13:14:09 +0800

    finish test case with another flow

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "2. Nama"
 // ...
 const sorter = (arr, callback) => {
-  const sortedArr = [...callback(arr)];
+  const sortedArr = callback(arr);
 
   for (let index = 0; index < sortedArr.length; index++) {
     sortedArr[index] = `${index + 1}. ${sortedArr[index]}`;
@@ -18,11 +18,32 @@ const sorter = (arr, callback) => {
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = (arr) => [...arr].sort();
+const sortAscending = (arr) => {
+  const newArr = [...arr];
+
+  for (let index = 0; index < newArr.length; index++) {
+    for (let deepIndex = index + 1; deepIndex < newArr.length; deepIndex++) {
+      if (newArr[deepIndex] < newArr[index]) {
+        [newArr[deepIndex], newArr[index]] = [newArr[index], newArr[deepIndex]];
+      }
+    }
+  }
+  
+  return newArr;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = (arr) => [...arr].sort().reverse();
+const sortDescending = (arr) => {
+  const sorted = sortAscending(arr);
+  const newArr = [];
+
+  for (let index = sorted.length - 1; index >= 0; index--) {
+    newArr.push(sorted[index]);
+  }
+
+  return newArr;
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
